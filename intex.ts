98f2993015e6f2a4790b3c024649b3e1ef0ef868
Majda_Brahimi <majda.brahimi@student-cs.fr>
2022-10-25 14:46:09 +0200

    extact guess

--- intex.ts
+++ intex.ts
@@ -1,7 +1,33 @@
 import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 
-async function handler(_req: Request): Promise<Response> {
-    return new Response("Hello World");
+async function handler(word1: string, word2: string): Promise<string> {
+    const body = {
+        sim1: word1,
+        sim2: word2,
+        lang: "fr",
+        type: "General Word2Vec",
+    };
+    const similarityResponse = await fetch(
+        "http://nlp.polytechnique.fr/similarityscore",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(body),
+        }
+    );
+    const similarityResponseJson = await similarityResponse.json();
+    return Number(similarityResponseJson.simscore).toString();
 }
 
-serve(handler);
+const extractGuess = async (req: Request) => {
+    const slackPayload = await req.formData();
+    const guess = await slackPayload.get("text")?.toString();
+    if (!guess) {
+        throw Error("Guess is empty or null");
+    }
+    return new Response(guess);
+};
+
+serve(extractGuess);
