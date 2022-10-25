c7eb21969ec0fb833b3cd9fb2eaceaeb57e21497
Majda_Brahimi <majda.brahimi@student-cs.fr>
2022-10-25 16:40:30 +0200

    clean version

--- intex.ts
+++ intex.ts
@@ -1,12 +1,47 @@
 import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 
-async function handler(word1: string, word2: string): Promise<string> {
+async function handler(_req: Request): Promise<Response> {
+    try {
+        const wordToFind = "chien";
+        const guess = await extractGuess(_req);
+        const similarityResult = await similarity(guess, wordToFind);
+        console.log(
+            `Tried with word ${guess}, similarity is ${similarityResult}, word to find is ${wordToFind}`
+        );
+        return new Response(responseBuilder(guess, similarityResult));
+    } catch (e) {
+        console.error(e);
+        return new Response("An error occured : ", e);
+    }
+}
+
+const extractGuess = async (req: Request) => {
+    const slackPayload = await req.formData();
+    const guess = await slackPayload.get("text")?.toString();
+    if (!guess) {
+        throw Error("Guess is empty or null");
+    }
+    return guess;
+};
+
+const responseBuilder = (word: string, similarity: Number) => {
+    if (similarity == 1) {
+        return `Well played ! The word was ${word}.`;
+    } else if (similarity > 0.5) {
+        return `${word} is very close to the word, score : ${similarity}`;
+    } else if (similarity < 0.5) {
+        return `${word} is quite far to the word, score : ${similarity}`;
+    }
+};
+
+const similarity = async (word1, word2) => {
     const body = {
         sim1: word1,
         sim2: word2,
         lang: "fr",
         type: "General Word2Vec",
     };
+    console.log("body", body);
     const similarityResponse = await fetch(
         "http://nlp.polytechnique.fr/similarityscore",
         {
@@ -17,21 +52,10 @@ async function handler(word1: string, word2: string): Promise<string> {
             body: JSON.stringify(body),
         }
     );
+    console.log("similarityResponse", similarityResponse);
     const similarityResponseJson = await similarityResponse.json();
-    return Number(similarityResponseJson.simscore).toString();
-}
-
-const extractGuess = async (req: Request) => {
-    let score: string;
-    const slackPayload = await req.formData();
-    const guess = await slackPayload.get("text")?.toString();
-    if (!guess) {
-        throw Error("Guess is empty or null");
-    }
-    else {
-        score = await handler(guess, 'chien');
-        return new Response(score);
-    }
+    console.log("similarityValue", similarityResponseJson);
+    return Number(similarityResponseJson.simscore);
 };
 
-serve(extractGuess);
+serve(handler);
